42a2c81102b9b0f82c7b00c6ad59ed63abaa014b
F9812 <uakreper1209@gmail.com>
2025-11-22 03:34:38 +0300

    Add event listeners for button clicks

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("message").textContent = "You clicked the first button!";
+});
+
+document.getElementById("button2").addEventListener("click", function() {
+    document.getElementById("message").textContent = "You clicked the second button!";
+});
